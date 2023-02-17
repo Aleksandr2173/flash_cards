3a81992be5b70be507481faee801b1c003dd2acc
EugeneAE <eremin.ea@gmail.com>
2023-02-17 13:04:54 +0300

    add read answer

--- read.js
+++ read.js
@@ -18,6 +18,22 @@ class ReadFile {
 
     return takeQues.flat();
   }
+  answers(path) {
+    let textA = fs
+      .readFileSync(`${path}`, 'utf-8')
+      .split('\n')
+      .filter((el) => el !== '')
+      .map((el) => el.split(','));
+    let takeAnsw = [];
+    for (let i = 0; i < textA.length; i++) {
+      console.log(textA[i / 2]);
+      if (textA[i / 2]) {
+        takeAnsw.push(textA[i]);
+      }
+    }
+    console.log(takeAnsw);
+    return takeAnsw.flat();
+  }
 }
 
 module.exports = ReadFile;

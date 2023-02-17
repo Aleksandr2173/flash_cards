e5de7ba2e64922a680f678dee89acc233b27a432
EugeneAE <eremin.ea@gmail.com>
2023-02-17 16:52:18 +0300

    ppp

--- read.js
+++ read.js
@@ -1,5 +1,6 @@
 const fsp = require('fs').promises;
 const fs = require('fs');
+const { removeAllListeners } = require('process');
 class ReadFile {
   questions(path) {
     let textQ = fs
@@ -37,3 +38,35 @@ class ReadFile {
 }
 
 module.exports = ReadFile;
+
+// const ret = fsp.readdir(`${__dirname}/topics`).then((el) => el);
+// console.log(ret);
+
+const retD = fs.readdirSync(`${__dirname}/topics`);
+// console.log(retD);
+fsp.readdir(`${__dirname}/topics/`).then((el) => console.log(el));
+
+const readF = fs.readFileSync(`${__dirname}/topics/${retD[0]}`, 'utf-8');
+// console.log(readF);
+let r = readF.split('\n');
+// console.log(r);
+
+// console.log(r.filter((el) => el !== '').map((el) => el.split(',')));
+
+let text = r.filter((el) => el !== '').map((el) => el.split(','));
+
+// console.log(
+//   text.map((el,i) => {
+//     el[i/2];
+//   })
+// );
+let rom = [];
+let roma = [];
+for (let i = 0; i < text.length; i++) {
+  //   console.log(text[i / 2 !== 0]);
+  if (text[i % 3]) {
+    rom.push(text[i]);
+  } else roma.push(text[i]);
+}
+console.log(rom.flat());
+console.log(roma.flat());
